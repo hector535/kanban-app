e12ec2866488f7cb714a1c42495e28c70ff07d4b
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-12-29 22:42:10 -0400

    Set body styles for light/dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,6 +80,9 @@ module.exports = {
           },
         ],
       },
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
