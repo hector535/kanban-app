2dcf5862f3af142f420726fd0d17b972f78c45d4
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-12-31 01:47:48 -0400

    Configure redux store and ui slice

---
+++ src/slices/ui-slice.ts
@@ -0,0 +1,40 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type UIState = {
+  dialogs: {
+    showBoardDeletion: boolean;
+    showTaskDeletion: boolean;
+    showTaskDetails: boolean;
+    showTaskForm: boolean;
+  };
+};
+
+type DialogVisibility = {
+  name: keyof UIState["dialogs"];
+  visibility: boolean;
+};
+
+const initialState: UIState = {
+  dialogs: {
+    showBoardDeletion: false,
+    showTaskDeletion: false,
+    showTaskDetails: false,
+    showTaskForm: false,
+  },
+};
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setDialogVisibility: (state, action: PayloadAction<DialogVisibility>) => {
+      const { name, visibility } = action.payload;
+
+      state.dialogs[name] = visibility;
+    },
+  },
+});
+
+export const { setDialogVisibility } = uiSlice.actions;
+
+export default uiSlice.reducer;
